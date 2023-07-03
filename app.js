2ec4b1aa073e9c0b5979f746ea4485baebbfad00
Ianm234 <126569965+Ianm234@users.noreply.github.com>
2023-07-03 09:59:25 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
-fetch('http://192.168.20.118:8080')
+
+fetch('http://127.0.0.1:8080')
   .then(response => response.json())
   .then(data => {
     
@@ -33,3 +34,39 @@ function displayMovieDetails(movie) {
   const ticketsAvailable = movie.capacity - movie.tickets_sold;
   availableTickets.textContent = `Available Tickets: ${ticketsAvailable}`;
 }
+
+const buyTicketButton = document.getElementById('buy-ticket');
+buyTicketButton.addEventListener('click', () => {
+  const selectedFilm = document.querySelector('.selected-film');
+  if (!selectedFilm) return;
+
+  const filmId = selectedFilm.dataset.filmId;
+  const ticketsSold = parseInt(selectedFilm.dataset.ticketsSold);
+
+  if (ticketsSold >= selectedFilm.dataset.capacity) {
+    alert('Tickets are sold out for this movie.');
+    return;
+  }
+
+  
+  const updatedTicketsSold = ticketsSold + 1;
+  fetch(`http://192.168.20.118:8080${filmId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      tickets_sold: updatedTicketsSold
+    })
+  })
+    .then(response => response.json())
+    .then(updatedMovie => {
+      
+      selectedFilm.dataset.ticketsSold = updatedTicketsSold;
+      const availableTickets = document.getElementById('available-tickets');
+      const ticketsAvailable = updatedMovie.capacity - updatedMovie.tickets_sold;
+      availableTickets.textContent = `Available Tickets: ${ticketsAvailable}`;
+    })
+    .catch(error => console.error(error));
+});
+
